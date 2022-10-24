99f69490bd4ed7fff3b72ca42bfadf56024c1ca7
Clinton <manakanejustin@gmail.com>
2022-10-24 14:16:25 +0200

    updated dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    container: false,
+    colors: {
+      secondary: "#FACD66",
+      primary: "#A4C7C6",
+      dark: "#1D2123",
+      "dark-alt": "#1A1E1F",
+      light: "#EFEEE0",
+      alt: "#A4C7C6",
+    },
     extend: {},
   },
   plugins: [],
